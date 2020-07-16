8ef33553fb69175bf5623c1107915779656cb60c
Ctyrone123 <Ctyrone123@gmail.com>
2020-07-16 15:32:57 -0500

    update template

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 // const pageHTML = generatePage(portfolioData)
 
 
@@ -137,5 +137,12 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData)
+
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+     });
   });
